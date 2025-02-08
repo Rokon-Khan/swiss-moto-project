f164a7ab61190901bf7b9eb045e6f936b73fa8d2
Rokon Khan <rokonjust@gmail.com>
2025-02-08 11:52:20 +0600

    AuthProvider Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,41 @@
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { RouterProvider } from "react-router-dom";
+// // import AuthProvider from "./authprovider/AuthProvider";
+// import "./index.css";
+// import { router } from "./routes/Routes";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <RouterProvider router={router}></RouterProvider>
+//     {/* <AuthProvider>
+//     </AuthProvider> */}
+//   </StrictMode>
+// );
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
-// import AuthProvider from "./authprovider/AuthProvider";
 import "./index.css";
+import AuthProvider from "./providers/AuthProvider";
 import { router } from "./routes/Routes";
 
+// Create a client
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-    {/* <AuthProvider>
-    </AuthProvider> */}
+    <AuthProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+        <Toaster position="top-right" reverseOrder={false} />
+      </HelmetProvider>
+    </AuthProvider>
   </StrictMode>
 );
